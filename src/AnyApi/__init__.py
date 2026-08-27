82dc8408e0b485697fa105625156c7eec0f8fc57
xXrederXx <morgenthalerthierry@gmail.com>
2026-08-27 20:15:59 +0200

    stuff

--- src/AnyApi/__init__.py
+++ src/AnyApi/__init__.py
@@ -1,11 +1,5 @@
 from .anytype import Anytype
-from .filters import Filter, FilterCondition, FilterExpression, FilterGroup, FilterOperator
 
 __all__ = [
 	"Anytype",
-	"Filter",
-	"FilterCondition",
-	"FilterExpression",
-	"FilterGroup",
-	"FilterOperator",
 ]
--- src/AnyApi/anytype.py
+++ src/AnyApi/anytype.py
@@ -1,6 +1,5 @@
 import requests
 
-from .filters import FilterExpression
 from .util.url import build_url
 
 from .model import AnySpacesResponse, AnySpace, AnyObject, AnyObjectsResponse
@@ -10,11 +9,13 @@ class Anytype:
     def __init__(self, api_key, base_url="http://127.0.0.1:31009"):
         self.base_url = base_url.rstrip("/")
         self.session = requests.Session()
-        self.session.headers.update({
-            'Accept': 'application/json',
-            "Authorization": f"Bearer {api_key}",
-            "Anytype-Version": "2025-11-08",
-        })
+        self.session.headers.update(
+            {
+                "Accept": "application/json",
+                "Authorization": f"Bearer {api_key}",
+                "Anytype-Version": "2025-11-08",
+            }
+        )
 
     def get(self, path, **kwargs):
         response = self.session.get(
@@ -30,22 +31,47 @@ class Anytype:
             json=data,
             **kwargs,
         )
-        print(data)
-        print(response.text)
         response.raise_for_status()
         return response.json()
 
-    def spaces(self, offset:int=0, limit:int=100) -> AnySpacesResponse:
-        return AnySpacesResponse.model_validate(self.get(build_url("v1", "spaces", offset=offset, limit=limit)))
+    def spaces(self, offset: int = 0, limit: int = 100) -> AnySpacesResponse:
+        return AnySpacesResponse.model_validate(
+            self.get(build_url("v1", "spaces", offset=offset, limit=limit))
+        )
 
     def space(self, space_id: str) -> AnySpace:
-        return AnySpace.model_validate(self.get(build_url("v1", "spaces", space_id))["space"])
-    
-    def objects(self, space_id:str, offset:int=0, limit:int=100) -> AnyObjectsResponse:
-        return AnyObjectsResponse.model_validate(self.get(build_url("v1", "spaces", space_id, "objects", offset=offset, limit=limit)))
-    
-    def object(self, space_id:str, object_id:str) -> AnyObject:
-        return AnyObject.model_validate(self.get(build_url("v1", "spaces", space_id, "objects", object_id))["object"])
-    
-    def search(self, filter:FilterExpression):
-        print(self.post(build_url("v1", "search"), filter.build()))
+        return AnySpace.model_validate(
+            self.get(build_url("v1", "spaces", space_id))["space"]
+        )
+
+    def objects(
+        self, space_id: str, offset: int = 0, limit: int = 100
+    ) -> AnyObjectsResponse:
+        return AnyObjectsResponse.model_validate(
+            self.get(
+                build_url(
+                    "v1", "spaces", space_id, "objects", offset=offset, limit=limit
+                )
+            )
+        )
+
+    def object(self, space_id: str, object_id: str) -> AnyObject:
+        return AnyObject.model_validate(
+            self.get(build_url("v1", "spaces", space_id, "objects", object_id))[
+                "object"
+            ]
+        )
+
+    def object_by_types(
+        self, type_keys: list[str], space_id: str | None = None
+    ) -> AnyObjectsResponse:
+        return AnyObjectsResponse.model_validate(
+            self.post(
+                (
+                    build_url("v1", "search")
+                    if space_id is None
+                    else build_url("v1", "spaces", space_id, "search")
+                ),
+                data={"types": type_keys},
+            )
+        )
